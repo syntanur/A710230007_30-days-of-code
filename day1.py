73adba21ae19374b4bf9223d259f04894cd854de
SyntaNur <A710230007@student.ums.ac.id>
2024-06-02 21:54:48 +0700

    first commit

---
+++ day1.py
@@ -0,0 +1,13 @@
+#mengambil inputan nama
+Nama=input('Siapa  Nama Kamu  : ')
+
+#mengambil inputan nim
+Nim=input('Berapa  Nim Kamu   : ')
+
+#mengambil inputan angkatan
+Angkatan= input ('Angkatan Berapa Kamu : ')
+
+#menampilkan variabel
+print('hallo',Nama )
+print('Nim kamu ',Nim)
+print('Angkatan kamu',Angkatan)
