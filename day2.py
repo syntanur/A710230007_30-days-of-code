3ae38dce96d1793ebc9771938ad71eb080140630
SyntaNur <A710230007@student.ums.ac.id>
2024-06-03 19:19:11 +0700

    day2.py

---
+++ day2.py
@@ -0,0 +1,9 @@
+#masukan inputan data
+bilangan = int(input(' masukkan bilangan :'))
+pangkat = int(input('masukan pangkat :'))
+
+#masukan inputan rumus
+hasil = bilangan ** pangkat
+
+#menampilkan variabel
+print ('hasil =',bilangan ** pangkat)
